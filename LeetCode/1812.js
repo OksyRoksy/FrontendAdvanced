4a1249978b68c4d2b81ac7710c209111841ac201
OksyRoksy <o.azarh@gmail.com>
2021-04-18 17:40:53 +0300

    new

---
+++ LeetCode/1812.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+ var squareIsWhite = function(coordinates) {
+    let first = "a";
+let second = "1";
+const map = {
+  a1: false,
+  a2: true,
+  b1: true,
+  b2: false,
+};
+if (!["a", "c", "e", "g"].includes(coordinates[0])) {
+  first = "b";
+}
+if (parseInt(coordinates[1]) % 2 == 0) {
+  second = "2";
+}
+return map[first + second];
+};
+console.log(squareIsWhite('a1'));
+console.log(squareIsWhite('b1'));
+console.log(squareIsWhite('c8'));
+console.log(squareIsWhite('d8'));
